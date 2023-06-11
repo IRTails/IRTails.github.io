95ffade2d06734b9cccf93f7147460a8b850319f
sh1dow3r <sh1dow3r@gmail.com>
2023-06-12 02:10:04 +0300

    Deploy website - based on aa72ed3d1b67a23ece282054d0a66cb8d6ac89ef

---
+++ assets/js/b2b675dd.760c73d1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkir_tails_github_io=self.webpackChunkir_tails_github_io||[]).push([[533],{8017:t=>{t.exports=JSON.parse('{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
